4d4e1349ae2161148fb9f22f0ac8dc95097bd8a0
JinhyunLee <leejinhyun99@naver.com>
2024-12-20 10:41:31 +0900

    Add sick_hack to band_list

---
+++ sick_hack.java
@@ -0,0 +1,7 @@
+public class sick_hack {
+    public static void main(String[] args) {
+        System.out.println("히로이 키쿠리");
+        System.out.println("이와시타 시마");
+        System.out.println("시미즈 일라이자");
+    }
+}
