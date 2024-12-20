22cc47327bdef2484acdf30ecf1e2c4e6aaf9788
JinhyunLee <leejinhyun99@naver.com>
2024-12-20 10:59:54 +0900

    Add member to guitar

---
+++ guitar.java
@@ -0,0 +1,10 @@
+public class guitar {
+    public static void main(String[] args) {
+        System.out.println("後藤ひとり");
+        System.out.println("喜多郁代");
+        System.out.println("시미즈 일라이자");
+        System.out.println("히로이 키쿠리");
+        System.out.println("오오츠키 요요코");
+        System.out.println("혼죠 후코");
+    }
+}
