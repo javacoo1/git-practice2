00379c4dd1b8d4c9b412d879ef0c142705b5d0ce
JinhyunLee <leejinhyun99@naver.com>
2024-12-20 10:45:06 +0900

    Add sideros to band_list

---
+++ sideros.java
@@ -0,0 +1,8 @@
+public class sideros {
+    public static void main(String[] args) {
+        System.out.println("오오츠키 요요코");
+        System.out.println("하세가와 아쿠비");
+        System.out.println("혼죠 후코");
+        System.out.println("우치다 유유");
+    }
+}
