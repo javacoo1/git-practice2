c66a03b8689f1c3b4d442edfdac8720aeb601c7c
JinhyunLee <leejinhyun99@naver.com>
2024-12-20 10:52:36 +0900

    Add member to singer

---
+++ singer.java
@@ -0,0 +1,7 @@
+public class singer {
+    public static void main(String[] args) {
+        System.out.println("喜多郁代");
+        System.out.println("히로이 키쿠리");
+        System.out.println("오오츠키 요요코");
+    }
+}
