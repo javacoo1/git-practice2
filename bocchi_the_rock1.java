200e3fc339098670258c3f08710b3b220386ea66
JinhyunLee <leejinhyun99@naver.com>
2024-12-20 10:11:22 +0900

    update boochi_the_rock

--- bocchi_the_rock1.java
+++ bocchi_the_rock1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class bocchi_the_rock1 {
     public static void main(String[] args) {
-    System.out.println("hitori bocchi");
+    System.out.println("結束バンド");
+    System.out.println("Sick Hack");
     }
 }
