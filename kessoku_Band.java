15b12da1fb7b448538edfe28148081f32659b45c
JinhyunLee <leejinhyun99@naver.com>
2024-12-20 10:37:52 +0900

    Add kessoku_Band to band_list

---
+++ kessoku_Band.java
@@ -0,0 +1,8 @@
+public class kessoku_Band {
+    public static void main(String[] args) {
+        System.out.println("後藤ひとり");
+        System.out.println("伊地知虹夏");
+        System.out.println("山田リョウ");
+        System.out.println("喜多郁代");
+    }
+}
